61cff06fa079e26ee9614b0092703a6603934c15
AglioB <126629977+AglioB@users.noreply.github.com>
2023-03-14 16:55:49 -0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,32 @@
+Config = {}
+
+Config.Ubicaciones = {
+
+    ["blip1"] = {														--El nombre del blip que solo es visible aqui
+        ["coords"] = vector3(-541.7056, -209.7303, 37.6498),			--Cordenadas del blip
+		["label"] = "Ayuntamiento",										--Nombre del blip que se muestra en pantalla
+		["showblip"] = true,											--Si se muestra o no el blip 
+        ["blipsprite"] = 475,											--El modelo del blip https://docs.fivem.net/docs/game-references/blips/#blips
+        ["blipscale"] = 1.0,											--El tamaño del blip
+        ["blipcolor"] = 46,												--El color del blip https://docs.fivem.net/docs/game-references/blips/#blip-colors
+    },
+
+	["blip2"] = {														--El nombre del blip que solo es visible aqui
+		["coords"] = vector3(255.5820, -738.4860, 38.9938),					--Cordenadas del blip
+		["label"] = "blip2",												--Nombre del blip que se muestra en pantalla
+		["showblip"] = true,												--Si se muestra o no el blip 
+		["blipsprite"] = 51,												--El modelo del blip https://docs.fivem.net/docs/game-references/blips/#blips
+		["blipscale"] = 1.0,												--El tamaño del blip
+		["blipcolor"] = 2,													--El color del blip https://docs.fivem.net/docs/game-references/blips/#blip-colors
+	},
+
+	["blip3"] = {														--El nombre del blip que solo es visible aqui
+		["coords"] = vector3(-819.0699, -464.4650, 33.9979),				--Cordenadas del blip
+		["label"] = "blip3",												--Nombre del blip que se muestra en pantalla
+		["showblip"] = true,												--Si se muestra o no el blip 
+		["blipsprite"] = 51,												--El modelo del blip https://docs.fivem.net/docs/game-references/blips/#blips
+		["blipscale"] = 1.0,												--El tamaño del blip
+		["blipcolor"] = 3,													--El color del blip https://docs.fivem.net/docs/game-references/blips/#blip-colors
+	},
+
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "AglioB"
+description "Blip Creator"
+
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+lua54 'yes'
